3182ba9a780f79134b60f2106be7d63a2731c739
Ruturaj <ruturajmengal2016@gmail.com>
2023-03-05 16:56:28 +0530

    commit successfully

---
+++ server.js
@@ -0,0 +1,16 @@
+const express = require("express");
+const errorHandler = require("./middleware/middleware");
+const routes = require("./Routes/router");
+require("dotenv").config()
+const app = express();
+app.use(express.json())
+app.use(errorHandler.errorHandler)
+app.use(errorHandler.applicationErrorHandler)
+app.use("/api",routes)
+
+
+app.listen(process.env.PORT, async (req,res)=>{
+    await require("./Config/Dbconnection")
+    console.log(`server is listening on ${process.env.PORT}`)
+})
+
