4b347079bf8c070c7d35f2536231f81b752a8938
Ruturaj <ruturajmengal2016@gmail.com>
2023-03-07 18:05:21 +0530

    second

--- server.js
+++ server.js
@@ -1,17 +1,26 @@
 const express = require("express"); //web framework
-const errorHandler = require("./Middleware/middleware"); //Middleware for error handler
+// const {errorHandler,applicationErrorHandler} = require("./Middleware/middleware"); //Middleware for error handler
 const routes = require("./Routes/router");  //Router
 require("dotenv").config() //env variables
 const app = express();
 app.use(express.json()) //body parser
-app.use(errorHandler.errorHandler) //error Handler
-app.use(errorHandler.applicationErrorHandler)
+const errorHandler = (err,req,res,next)=>{
+    console.error(err.stack)
+    res.status(500).send("Something Wrong!")
+}
+
+const applicationErrorHandler = (req, res, next) => {
+    console.log('Time:', Date.now());
+    next();
+  }
+app.use(errorHandler) //error Handler
+app.use(applicationErrorHandler)
 app.use("/api",routes) // api routes
 
 
 
 //server and database
-app.listen(process.env.PORT, async (req,res)=>{
+app.listen(process.env.PORT, async ()=>{
     await require("./Config/Dbconnection")
     console.log(`server is listening on ${process.env.PORT}`)
 })
